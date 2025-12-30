48193a0872dc46cd2ea300bb7b7913ad38aa8f3b
soeun823 <bsoeun866@gmail.com>
2025-12-30 11:52:35 +0900

    feat : 화살표 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import { useGameStore } from '../stores/gameStore';
-import { Button } from '../components/ui';
 import { getRandomBackground } from '../utils/randomBackground';
+import Arrow from "../assets/arrow.svg"
 
 const MainPage: React.FC = () => {
   const setCurrentPage = useGameStore(state => state.setCurrentPage);
@@ -21,16 +21,17 @@ const MainPage: React.FC = () => {
 
       <div className="max-w-4xl mx-auto text-center relative z-10">
         {/* 헤더 */}
-          <div className="flex items-center justify-center mb-6">
-            <h1 className="text-5xl font-bold text-white">
-             화면을 클릭해서 게임을 시작하세요
-            </h1>
-
+          <div className="flex flex-col justify-center items-center justify-center mb-2">
+            <div>
+                <h1 className="text-4xl font-bold text-white">
+                  화면을 클릭해서 게임을 시작하세요
+                </h1>
+                <p className="mt-4 text-sm text-white opacity-70 mb-2">
+                  소요시간: 약 5-10분 | 무료 체험
+                </p>
+            </div>
+            <img className="w-10 arrow-float" src={Arrow} alt={"화살표"}/>
           </div>
-
-          <p className="mt-4 text-sm text-white opacity-70">
-            소요시간: 약 5-10분 | 무료 체험
-          </p>
 
       </div>
 
@@ -39,6 +40,7 @@ const MainPage: React.FC = () => {
         <p className="text-sm text-white opacity-70">
           안전한 해양활동을 위한 교육용 시뮬레이션입니다
         </p>
+
       </div>
     </div>
   );
